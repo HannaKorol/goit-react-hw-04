50b37236a9a29146c359b359156a0310a430c535
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-10-05 11:50:20 +0200

    edit app and api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useState } from "react";
-import { SearchBar } from "./SearchBar/SearchBar.jsx";
 import Loader from "./Loader/Loader.jsx";
 import ErrorMessage from "./ErrorMessage/ErrorMessage.jsx";
 import fetchImages from "../Services/api.js";
+import SearchBar from "./SearchBar/SearchBar.jsx";
+import ImageGallery from "./ImageGallery/ImageGallery.jsx";
 
 //1)HTTP-запити можна виконувати як за подією(при кліку на елементі чи відправці форми) або при монтажі компонента. 2-варіант - використовується ефект - оскільки компонент є в ДОМ і готовий оновлювати стан. Але Оскільки тепер користувач сам вводить рядок для пошуку статей, нам не потрібний ефект.
 //2)Форма пошуку рендериться в компоненті App, а функція handleSearch буде відповідати за код, який необхідно виконати при сабміті форми.
 
 export default function App() {
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState([]); //is initialized as an empty array because it will store the data fetched from the API and then show it in our Result Section
   const [loading, setLoading] = useState(false); // lectioon 1 (50:28)
   const [error, setError] = useState(false);
   const [page, setPage] = useState(0);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState(""); //it will be used to store the input from the search bar (and that is also a string).
 
   //вся логіка запиту і обробки службового стейту (isLoading, error) виконуєится в useEffect з залежностями [query, page]
+  
   //Функція буде викликана кожен раз при зміна номера сторінки:
   useEffect(() => {
     if (!query) {
@@ -22,11 +24,11 @@ export default function App() {
     }
     const getData = async () => {
       try {
-        setError(false);             // при новому запиті помилка зникає
+        setError(false); // при новому запиті помилка зникає
         setLoading(true);
         const data = await fetchImages(page, query);
         setLoading(false);
-        setImages(prev => [...prev, ...data.hits]);
+        setImages((prev) => [...prev, ...data.hits]);
       } catch {
         setError(true);
       } finally {
@@ -37,15 +39,15 @@ export default function App() {
   }, [page, query]);
 
   const handleChangePage = () => {
-    setPage(prev => prev +1 )          //коли натискають на кнопку - збільшується стейт
-  }
+    setPage((prev) => prev + 1); //коли натискають на кнопку - збільшується стейт
+  };
 
   //Скидаємо збереженні сторінки і запити пошуку:
-  const handleSetQuery = (topic) {
+  const handleSetQuery = (topic) => {
     setQuery(topic);
-    setImages([]); 
+    setImages([]);
     setPage(0); //скидання сторінок якщо ми шукаємо по іншій темі пошуку
-}
+  };
 
   return (
     <div>
@@ -57,7 +59,7 @@ export default function App() {
     </div>
 
     // Відображення номера сторінки для вибору !! Корисна відмальовка
-   /*  <div>
+    /*  <div>
    {Array(10)
    .fill('')
    .map((_, i) =>(   //(item, index, array, _ означає що це не потрібно)
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
 
-export default function fetchImages= async (page = 0, query) => {
-    const { data } = await axios.get();
-    return data;
+const fetchImages = async (page = 0, query) => {
+    const { data } = await axios.get(`http://api.unsplash.com/search/photos`, {
+        params: {
+            client_id:"ijWjyKNt7NfEKBD9u9_g_GPxgVxqtsRpn_FFbW5wvNA",
+            query: query,
+            page: page,
+            per_page: 7,
+        },
+    });
+    return data.results;
 };
+
+export default fetchImages;

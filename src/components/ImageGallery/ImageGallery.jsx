ad3c63c29527c95d560cc66a75c96ff5083cbf53
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-10-06 14:05:53 +0200

    add dependancies from ImageModal

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import ImageCard from "../ImageCard/ImageCard";
 import s from "./ImageGallery.module.css"
+import ImageModal from "../ImageModal/ImageModal";
 
-const ImageGallery = ({images}) => {
+const ImageGallery = ({ images }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleImageClick = (imageUrl) => {
+    selectedImage(imageUrl);
+    setIsModalOpen(true);
+  };
+
+  const hundleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage(null);
+  };
+
   return (
+     <div>
     <ul className={s.list}>
       {images.map((image) => (
-        <li key={image.id + image.created_at}>
+        <li key={image.id} onClick={() => handleImageClick(image.urls.large)}>
           <ImageCard image={image} />
         </li>
       ))}
     </ul>
+      {selectedImage && (<ImageModal isOpen={isModalOpen} onRequestClose={hundleCloseModal} imageUrl={selectedImage}/> 
+      )}
+  </div> 
   );
 };
 
